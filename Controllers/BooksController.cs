95f8f2d8abb5a49410b977bac847bc84c051e2bc
IT21112300 <it21112300@my.sliit.lk>
2025-04-01 16:38:06 +0530

    Basic CRUD added for Books

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookstoreAPI.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public BooksController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("booklist")]
+    public ActionResult<IEnumerable<Book>> GetBooks()
+    {
+        return _context.Books.ToList();
+    }
+
+    [HttpPost("addBook")]
+    public ActionResult<Book> AddBook(Book book)
+    {
+        _context.Books.Add(book);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetBooks), new { id = book.Id }, book);
+    }
+
+    [HttpPut("updateBook/{id}")]
+    public IActionResult UpdateBook(int id, Book updatedBook)
+    {
+        var book = _context.Books.Find(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        book.Title = updatedBook.Title;
+        book.Author = updatedBook.Author;
+        book.Price = updatedBook.Price;
+
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("deleteBook/{id}")]
+    public IActionResult DeleteBook(int id)
+    {
+        var book = _context.Books.Find(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        _context.Books.Remove(book);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookstoreAPI.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+}
